e5c4979cf24fccad8e7d9367344bfc5f1bb8e7ae
Kujonick <przemek.r@opoczta.pl>
2023-10-24 17:36:30 +0200

    matrix inverse

---
+++ zestaw2/Matrix_inverse.py
@@ -0,0 +1,27 @@
+from Strassen import SMU as mul
+from Number import Number
+import numpy as np
+def inverse(A) -> np.array:
+    n = len(A)
+    if n == 1:
+        a = A[0][0]
+        if a != 0:
+            return np.array([[1/a]], dtype=Number)
+        else:
+            raise ValueError("Matrix is not invertilbe")
+    n//=2
+    A_11, A_12, A_21, A_22 = A[:n,:n], A[:n,n:], A[n:,:n],A[n:,n:]
+    invA_11 = inverse(A_11)
+    S = A_22 - mul(mul(A_21, invA_11),A_12) 
+    invS = inverse(S)
+    C = np.zeros((2*n,2*n), dtype=Number)
+    C[:n,:n] = invA_11 + mul(mul(mul(invA_11,A_12), invS),mul(A_21, invA_11))
+    C[:n,n:] = -1 * mul(mul(invA_11,A_12), invS)
+    C[n:,:n] = -1 * mul(mul(invS,A_21), invA_11)
+    C[n:,n:] = invS
+    return C
+
+if __name__ == "__main__":
+    A = np.array([[Number(2),Number(3)],[Number(1),Number(2)]], dtype=Number)
+    print(inverse(A))
+
---
+++ zestaw2/Strassen.py
@@ -0,0 +1,28 @@
+from Number import Number
+import numpy as np
+
+def SMU(A,B): # Strassen Matrix Multiplication
+    if len(A) == 1:
+        return A@B
+    n= len(A)//2
+    A_11, A_12, A_21, A_22 = A[:n,:n], A[:n,n:], A[n:,:n],A[n:,n:]
+    B_11, B_12, B_21, B_22 = B[:n,:n], B[:n,n:], B[n:,:n],B[n:,n:]
+    M1 = SMU(A_11+A_22, B_11+B_22)
+    M2 = SMU(A_21+A_22, B_11)
+    M3 = SMU(A_11, B_12-B_22)
+    M4 = SMU(A_22, B_21-B_11)
+    M5 = SMU(A_11 + A_12, B_22)
+    M6 = SMU(A_21-A_11, B_11+B_12)
+    M7 = SMU(A_12-A_22, B_21+B_22)
+    C = np.zeros((2*n,2*n), dtype=Number)
+    C[:n,:n] = M1 + M4 - M5 + M7
+    C[:n,n:] = M3 + M5
+    C[n:,:n] = M2 + M4
+    C[n:,n:] = M1 - M2 + M3 + M6
+    return C
+
+if __name__ == "__main__":
+    A = np.array([[Number(5),Number(2)],[Number(1),Number(1)]], dtype=Number)
+    B = np.array([[Number(1),Number(2)],[Number(1),Number(-3)]], dtype=Number)
+    print(SMU(A,B))
+    print(Number.ADD_counter)
