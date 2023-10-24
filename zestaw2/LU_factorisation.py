b97f4c930c6ea108cbba61d783be07453bc95b6c
Kujonick <przemek.r@opoczta.pl>
2023-10-24 18:38:57 +0200

    algorythms

--- zestaw2/LU_factorisation.py
+++ zestaw2/LU_factorisation.py
@@ -1,2 +1,25 @@
+import numpy as np
+from Number import Number
+from reverse_matrix import reverse_matrix
+from Strassen import SMU as mul
+
 def LU_factorise(A):
-    pass
+    if len(A) == 1:
+        return np.ones((1,1),dtype=Number), A
+    n = len(A)//2
+    A_11, A_12, A_21, A_22 = A[:n,:n], A[:n,n:], A[n:,:n],A[n:,n:]
+    L_11, U_11 = LU_factorise(A_11)
+    invU_11 = reverse_matrix(U_11)
+    L_21 = mul(A_21, invU_11)
+    invL_11 = reverse_matrix(L_11)
+    U_12 = invL_11
+    S = A_22 - mul(mul(A_21, invU_11), mul(invL_11, A_12))
+    L_22, U_22 = LU_factorise(S)
+    L, U = np.zeros((2*n,2*n), dtype=Number), np.zeros((2*n,2*n), dtype=Number)
+    L[:n,:n], L[n:,:n], L[n:,n:] = L_11, L_21, L_22
+    U[:n,:n], U[:n,n:], U[n:,n:] = U_11, U_12, U_22
+    return L, U
+
+if __name__ == "__main__":
+    A = np.array([[Number(2),Number(1)],[Number(4),Number(2)]], dtype=Number)
+    print(LU_factorise(A))
--- zestaw2/recursive_det.py
+++ zestaw2/recursive_det.py
@@ -1,2 +1,13 @@
+import numpy as np
+from LU_factorisation import LU_factorise
+from Number import Number
 def recursive_det(A):
-    pass
+    L, U = LU_factorise(A)
+    return U.diagonal().prod()
+
+
+if __name__ == "__main__":
+
+    A = np.array([[Number(2),Number(1)],[Number(1),Number(2)]], dtype=Number)
+    print(LU_factorise(A)[0],'\n\n', LU_factorise(A)[1])
+    print(recursive_det(A))
