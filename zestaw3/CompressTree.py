5ecf12a0581aa875f4a7e58ce851aafcd0f60bcc
Kujonick <przemek.r@opoczta.pl>
2023-11-24 16:28:02 +0100

    poprawka

--- zestaw3/CompressTree.py
+++ zestaw3/CompressTree.py
@@ -2,7 +2,6 @@ from sklearn.utils.extmath import randomized_svd
 import numpy as np
 from numpy.linalg import svd
 
-
 class CompressTree:
     def __init__(self,matrix, row_min, row_max, col_min, col_max):
         self.matrix = matrix
@@ -30,7 +29,7 @@ class CompressTree:
     def create_tree(self, r, epsylon):
         
         U, Sigma, V = randomized_svd(self.matrix[self.row_min:self.row_max, self.col_min: self.col_max], n_components=r+1)
-        if self.row_max == self.row_min + 1:
+        if self.row_max == self.row_min + r:
             self.make_leaf(U, Sigma, V)
         elif Sigma[r] < epsylon:
             self.make_leaf(U, Sigma, V)
@@ -42,6 +41,20 @@ class CompressTree:
                       self.childs[i][j] = CompressTree(self.matrix, rows[i], rows[i+1], cols[j], cols[j+1])
                       self.childs[i][j].create_tree(r, epsylon)
 
+    def decompress(self, dest_matrix):
+        if self.leaf:
+            r = len(self.s)
+            sigma = np.zeros((r,r))
+            np.fill_diagonal(sigma, self.s)
+            dest_matrix[self.row_min:self.row_max, self.col_min: self.col_max] = self.u @ sigma @ self.v
+        
+        else:
+            for i in range(2):
+                for j in range(2):
+                    self.childs[i][j].decompress(dest_matrix)
+
+    def compare(self, new_matrix):
+        return np.sum(np.square(self.matrix - new_matrix))
 
 if __name__ == "__main__":
     X = np.random.random((64,64))
---
+++ zestaw3/TreeDraw.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import deque
+
+def draw_tree(root):
+    image = np.ones(root.matrix.shape)*255
+
+    Q = deque()
+    Q.append(root)
+    while Q:
+        v = Q.pop()
+        if v.leaf:
+            r = len(v.s)
+            gray = 125
+            image[v.row_min:v.row_max, v.col_min:v.col_min+r] = gray*np.ones((v.row_max - v.row_min, r))#np.zeros((v.row_max - v.row_min, min(r,v.col_max - v.col_min )))
+            image[v.row_min:v.row_min + r, v.col_min:v.col_max] =gray*np.ones((r , v.col_max - v.col_min)) #np.zeros((min(r,v.row_max - v.row_min) , v.col_max - v.col_min))
+            image[v.row_min, v.col_min:v.col_max] = np.zeros((1,v.col_max - v.col_min))
+            image[v.row_max-1, v.col_min:v.col_max] = np.zeros((1,v.col_max - v.col_min))
+            image[v.row_min:v.row_max,v.col_min] = np.zeros(v.row_max-v.row_min)
+            image[v.row_min:v.row_max,v.col_max-1] = np.zeros(v.row_max-v.row_min)
+        else:
+            for i in range(2):
+                for j in range(2):
+                    Q.append(v.childs[i][j])
+    
+
+
+    plt.imshow(image,cmap = "gray", vmin=0, vmax=255)
+    plt.show()
